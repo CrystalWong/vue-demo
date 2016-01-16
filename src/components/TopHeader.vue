38c0aa288c2d63a49e4357da581001fae5929565
zongkelong <zongkelong@gmail.com>
2016-01-16 21:16:31 +0800

    add floor1

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,10 +1,17 @@
 <template>
-    <header>
-        <span>{{hTitle}}</span>
+    <header class="uk-width-1-1 top-header">
+        <h1 class="uk-container-center uk-text-center uk-width-1-2" v-text="hTitle"></h1>
     </header>
 </template>
 <style>
-
+    .top-header{
+        height: 3em;
+        background: #ed724d;
+    }
+    .top-header h1{
+        color: #ffffff;
+        font-size: 1.5em;
+    }
 </style>
 <script>
 export default {
--- src/components/floor_type_1.vue
+++ src/components/floor_type_1.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>i am floor1</div>
+    <div v-for="ware in wares" class="uk-width-1-1 floor-1">
+        <img :src="ware.wareImg" class="uk-responsive-height" />
+        <p>{{ware.wareName}}</p>
+        <p>{{ware.warePrice}}</p>
+    </div>
 </template>
 
 <style>
-
+  .floor-1{
+      height:8em;
+      border-bottom:1px solid grey;
+  }
 </style>
 
 <script>
-
+    export default {
+        props: ['wares']
+    }
 </script>
