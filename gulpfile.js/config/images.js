4fdfc18525938d62537fa4c700063fc624610110
gaoshoushuai <shoushuai.gao@dmall.com>
2016-01-20 15:08:37 +0800

    modify  images task

--- gulpfile.js/config/images.js
+++ gulpfile.js/config/images.js
@@ -1,6 +1,6 @@
 var config = require('./')
 
 module.exports = {
-    src: config.sourceAssets + "/style/**",
-    dest: config.publicAssets + "/style"
+    src: config.sourceAssets + "/images/**",
+    dest: config.publicAssets + "/images"
 }
