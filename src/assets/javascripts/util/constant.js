a814a3ca2fd8633381f484a21eb5d5ed91637194
huanxing.wei <huanxing.wei@dmall.com>
2016-01-22 15:29:35 +0800

    update.

---
+++ src/assets/javascripts/util/constant.js
@@ -0,0 +1,12 @@
+let prefix = 'onepiece.'
+let constant = {
+  COOKIEPREFIX: prefix,
+  COOKIEDOMAIN: 'chuck.dmall.com',
+  LAT: prefix + 'lat',
+  LNG: prefix + 'lng',
+  STOREID: prefix + 'storeId',
+  VENDORID: prefix + 'venderId',
+  USERTEMPID: prefix + '.userTempId'
+}
+
+module.exports = constant
